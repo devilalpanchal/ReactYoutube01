9f8f2259908167081455ee5bacc6dda3271acc25
Devilal panchal <devilalpanchal551@gmail.com>
2024-04-27 10:10:49 +0530

    use memo

--- src/Hooks/UseMemo.js
+++ src/Hooks/UseMemo.js
@@ -1,22 +1,28 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
-function Memo(){
+function Memo() {
 
 
-    const [count ,setCount] = useState(0)
-    const [item ,setItem] = useState(10)
+    const [count, setCount] = useState(0)
+    const [item, setItem] = useState(10)
 
+
+    const MulticountUseMemo = useMemo(function MultiCount() {
+            console.log(" Multi Count")
+            return count * 5
+        },[count])
     return (
 
         <>
-        
-        <div className="App">
-<h2>  memo Hook to use  returns a memoized value and prevents the application from unnecessary re-renders</h2>
-<h3>{count}</h3>
-<button onClick={()=> setCount(count+1)}> Update count</button>
-<h3>{item}</h3>
-<button onClick={()=> setItem(item * 10)}> Update item</button>
-        </div>
+
+            <div className="App">
+                <h2>  memo Hook to use  returns a memoized value and prevents the application from unnecessary re-renders</h2>
+                <h3>{count}</h3>
+                <h2>{MulticountUseMemo}</h2>
+                <button onClick={() => setCount(count + 1)}> Update count</button>
+                <h3>{item}</h3>
+                <button onClick={() => setItem(item * 10)}> Update item</button>
+            </div>
         </>
     )
 }
